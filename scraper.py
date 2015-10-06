da246ca400a6cc5c57830402c8f36b902b70186f
Ge Tadeo <mr.genesis.tadeo@gmail.com>
2015-10-07 03:54:05 +0800

    Adds read and write to csv

--- scraper.py
+++ scraper.py
@@ -1,20 +1,54 @@
+import sys
 from bs4 import BeautifulSoup
 import requests
 import utils
+import csv
 
+reload(sys)
+sys.setdefaultencoding("utf8")
 
-url = "http://www.gsmarena.com/xiaomi_redmi_note_2-6992.php"
+with open('urls.csv', 'rb') as f:
+    rows = csv.reader(f)
+    urls = [r[0] for r in rows]
+    urls = urls[1:]
+
 DEFAULT_HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
 }
 
-
-r = requests.get(url, headers=utils.merge(DEFAULT_HEADERS, {}))
-soup = BeautifulSoup(r.text)
-
-
-for t in soup.select('table'):
-    section = t.select('th')[0].contents[0]
-    h = [e.contents[0] for e in t.select('.ttl > a')]
-    c = [e.contents[0] for e in t.select('.nfo')]
-    print dict(zip(h, c))
+with open('sample.csv', 'w') as csvfile:
+    fieldnames = ['Model', 'GPRS', '2G bands', 'Speed', '3G bands', 'EDGE',
+            'Technology', 'Status', 'SIM', '4G bands', 'Announced', 
+            'Dimensions', 'Weight', 'Resolution', 'Multitouch', 'Type', 'Size',
+            'Chipset', 'OS', 'CPU', 'GPU', 'Internal', 'Card slot', 
+            'Secondary', 'Video', 'Primary', 'Features', 'Loudspeaker', 
+            '3.5mm jack', 'Alert types', 'WLAN', 'USB', 'Infrared port', 
+            'Bluetooth', 'Radio', 'GPS', 'Messaging', 'Sensors', 'Java', 
+            'Browser', 'Talk time', 'Stand-by', 'Music play', 'Price group', 
+            'Colors', 'Battery life', 'Camera', 'Audio quality', 'Performance', 
+            'Display', 'Phonebook', 'Call records', 'Games', 'SAR EU', 
+            'SAR US', 'Protection', 'Keyboard', 'NFC']
+    writer = csv.DictWriter(
+        csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
+    writer.writeheader()
+    for u in urls:
+        r = requests.get(u, headers=utils.merge(DEFAULT_HEADERS, {}))
+        soup = BeautifulSoup(r.text, 'lxml')
+        data = {}
+        for t in soup.select('table'):
+            h = []
+            for e in t.select('.ttl > a'):
+                h.append(e.contents[0])
+            c=[]
+            for e in t.select('.nfo'):
+                if e.contents:
+                    c.append(e.contents[0])
+            section = t.select('th')[0].contents[0]
+            #h = [e.contents[0] for e in t.select('.ttl > a')]
+            #c = [str(e.contents[0]).encode('utf-8') for e in t.select('.nfo')]
+            data.update(dict(zip(h, c)))
+        title = soup.select('.specs-phone-name-title')[0].get_text()
+        data.update({'Model': title})
+        data['Technology'] = str(data['Technology']).strip('<a class="link-network-detail collapse" href="#"></a>')
+        print data
+        writer.writerow(data)

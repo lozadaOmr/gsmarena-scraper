f0c0c670dfae1e35cd1cf6e668e4125260718a4d
Carl Isip <carl.loiuse@gmail.com>
2016-03-03 02:11:22 -0800

    store data to database

--- scraper.py
+++ scraper.py
@@ -1,66 +1,122 @@
 import argparse
+import csv
+import re
+import requests
 import sys
 import socks
 import socket
-from bs4 import BeautifulSoup
-import requests
 import utils
-import csv
+
+from bs4 import BeautifulSoup
 from google import search
+from json import dumps, loads, JSONEncoder, JSONDecoder
+from sqlalchemy import create_engine, Column, Integer, String, Text, or_
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from urllib import quote
+
 
 reload(sys)
 sys.setdefaultencoding("utf8")
 
 # [TODO] put another scraper type
 # Get parser type using argparse
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('-t', '--type',
-                    default='urls',
-                    choices=['urls', 'models'],
-                    help="""
-Type:
-    1. urls (default) - checks filtered_urls.csv for links. Should be a valid
-        gsmarena link. Format: link
-    2. model - checks filtered_models.csv for manufacturer + model. Searches
-        model to google to return valid gsmarena link.
-        Format: manufacturer,model
-                    """.strip())
+parser = argparse.ArgumentParser(description='Scrapes data from gsmarena.com')
+parser.add_argument('-u', '--username',
+                    default='root',
+                    help="DB Username")
+parser.add_argument('-w', '--password',
+                    default='root',
+                    help="DB Password")
+parser.add_argument('-H', '--host',
+                    default='localhost',
+                    help="DB Host")
+parser.add_argument('-P', '--port',
+                    default=3306,
+                    help="DB Port")
+parser.add_argument('-d', '--database',
+                    default='test',
+                    help="DB Name")
 
 args = parser.parse_args()
 
-if args.type == 'models':
-    with open('filtered_urls.csv', 'wb') as fw:
-        writer = csv.DictWriter(fw, fieldnames=['link'],
-                                quoting=csv.QUOTE_ALL)
-        writer.writeheader()
+# mysql engine url
+engine_url = "mysql://%s:%s@%s:%s/%s" % (
+    args.username,
+    args.password,
+    args.host,
+    args.port,
+    args.database)
+engine = create_engine(engine_url)
 
-        with open('filtered_models.csv', 'rb') as fp:
-            rows = csv.DictReader(fp)
-            for row in rows:
-                manufacturer = row['manufacturer'].lower()
-                model = row['model'].lower()
+# create session
+Session = sessionmaker(bind=engine)
+session = Session()
 
-                # form keyword
-                keyword = "gsmarena %s %s" % (manufacturer, model)
+# create tables
+Base = declarative_base()
 
-                # google returns an iterator
-                query = search(keyword, num=1, stop=1)
+class Device(Base):
+    __tablename__ = 'device'
 
-                print "google search: %s" % keyword
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    manufacturer = Column(String(255))
+    model = Column(String(255))
+    meta = Column(Text)
+    url = Column(String(255))
 
-                try:
-                    for url in query:
-                        if ("www.gsmarena.com/%s" % manufacturer) in url:
-                            writer.writerow({'link': url})
-                        break
-                except Exception, e:
-                    print "warning:", e
+Base.metadata.create_all(engine)
 
-# always do this
-with open('filtered_urls.csv', 'rb') as f:
-    rows = csv.reader(f)
-    urls = [r[0] for r in rows]
-    urls = urls[1:]
+# fill all items without url
+query = session.query(Device).filter(Device.url.is_(None))
+
+for item in query.all():
+    manufacturer = item.manufacturer.lower()
+    model = item.model.lower()
+
+    # form keyword
+    keyword = "gsmarena %s %s" % (manufacturer, model)
+
+    # use bing to search for results
+    url = "http://www.bing.com/search?q={0}&go=Submit&qs=n&form=QBRE&pq={0}&sc=8-25&sp=-1&sk=&cvid=505D5E8D48744189BB96C5EC6FB91FB2".format(quote(keyword))
+
+    print "bing search: %s" % keyword
+
+    # get response
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print "warning:", response.content
+        continue
+
+    # match format
+    master = "http://www.gsmarena.com/%s" % manufacturer
+    content = response.content
+    preg = 'href="%s(.*?)"' % master
+
+    match = re.search(preg, content)
+
+    if match:
+        item.url = "%s%s" % (master, match.groups()[0])
+    else:
+        item.url = "n/a"
+    session.commit()
+
+    # # google returns an iterator
+    # query = search(keyword, num=1, stop=1)
+
+    # print "google search: %s" % keyword
+
+    # try:
+    #     for url in query:
+    #         if ("www.gsmarena.com/%s" % master) in url:
+    #             item.url = url
+    #         else:
+    #             item.url = "n/a"
+    #         session.commit()
+    #         break
+    # except Exception, e:
+    #     print "warning:", e
 
 socks.setdefaultproxy(proxy_type=socks.PROXY_TYPE_SOCKS5,
         addr='127.0.0.1', port=9050)
@@ -71,46 +127,55 @@ DEFAULT_HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
 }
 
-with open('sample.csv', 'w') as csvfile:
-    fieldnames = ['Model', 'GPRS', '2G bands', 'Speed', '3G bands', 'EDGE',
-            'Technology', 'Status', 'SIM', '4G bands', 'Announced',
-            'Dimensions', 'Weight', 'Resolution', 'Multitouch', 'Type', 'Size',
-            'Chipset', 'OS', 'CPU', 'GPU', 'Internal', 'Card slot',
-            'Secondary', 'Video', 'Primary', 'Features', 'Loudspeaker',
-            '3.5mm jack', 'Alert types', 'WLAN', 'USB', 'Infrared port',
-            'Bluetooth', 'Radio', 'GPS', 'Messaging', 'Sensors', 'Java',
-            'Browser', 'Talk time', 'Stand-by', 'Music play', 'Price group',
-            'Colors', 'Battery life', 'Camera', 'Audio quality', 'Performance',
-            'Display', 'Phonebook', 'Call records', 'Games', 'SAR EU',
-            'SAR US', 'Protection', 'Keyboard', 'NFC', 'Build', 'Alarm',
-            'Clock', 'Languages']
-    writer = csv.DictWriter(
-        csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
-    writer.writeheader()
-    for u in urls:
-        print 'url', u
-        r = requests.get(u, headers=utils.merge(DEFAULT_HEADERS, {}))
-        soup = BeautifulSoup(r.text, 'lxml')
-        data = {}
-        for t in soup.select('table'):
-            h = []
-            for e in t.select('.ttl > a'):
-                h.append(e.contents[0])
-            c=[]
-            for e in t.select('.nfo'):
-                if e.contents:
-                    c.append(e.contents[0])
-            section = t.select('th')[0].contents[0]
-            #h = [e.contents[0] for e in t.select('.ttl > a')]
-            #c = [str(e.contents[0]).encode('utf-8') for e in t.select('.nfo')]
-            data.update(dict(zip(h, c)))
-        try:
-            title = soup.select('.specs-phone-name-title')[0].get_text()
-        except Exception, e:
-            print "warning: possible wrong link"
-            title = u
-        data.update({'Model': title})
-        if 'Technology' in data:
-            data['Technology'] = str(data['Technology']).strip('<a class="link-network-detail collapse" href="#"></a>')
-        print data
-        writer.writerow(data)
+fieldnames = ['Model', 'GPRS', '2G bands', 'Speed', '3G bands', 'EDGE',
+        'Technology', 'Status', 'SIM', '4G bands', 'Announced',
+        'Dimensions', 'Weight', 'Resolution', 'Multitouch', 'Type', 'Size',
+        'Chipset', 'OS', 'CPU', 'GPU', 'Internal', 'Card slot',
+        'Secondary', 'Video', 'Primary', 'Features', 'Loudspeaker',
+        '3.5mm jack', 'Alert types', 'WLAN', 'USB', 'Infrared port',
+        'Bluetooth', 'Radio', 'GPS', 'Messaging', 'Sensors', 'Java',
+        'Browser', 'Talk time', 'Stand-by', 'Music play', 'Price group',
+        'Colors', 'Battery life', 'Camera', 'Audio quality', 'Performance',
+        'Display', 'Phonebook', 'Call records', 'Games', 'SAR EU',
+        'SAR US', 'Protection', 'Keyboard', 'NFC', 'Build', 'Alarm',
+        'Clock', 'Languages']
+
+# json serialize class
+class PythonObjectEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (list, dict, str, unicode, int, float, bool, type(None))):
+            return JSONEncoder.default(self, obj)
+        return {'_python_object': str(obj)}
+
+query = session.query(Device).filter(Device.url != "n/a")
+
+for item in query.all():
+    u = item.url
+    print 'url', u
+    r = requests.get(u, headers=utils.merge(DEFAULT_HEADERS, {}))
+    soup = BeautifulSoup(r.text, 'lxml')
+    data = {}
+    for t in soup.select('table'):
+        h = []
+        for e in t.select('.ttl > a'):
+            h.append(e.contents[0])
+        c=[]
+        for e in t.select('.nfo'):
+            if e.contents:
+                c.append(e.contents[0])
+        section = t.select('th')[0].contents[0]
+        #h = [e.contents[0] for e in t.select('.ttl > a')]
+        #c = [str(e.contents[0]).encode('utf-8') for e in t.select('.nfo')]
+        data.update(dict(zip(h, c)))
+    try:
+        title = soup.select('.specs-phone-name-title')[0].get_text()
+    except Exception, e:
+        print "warning: possible wrong link"
+        title = u
+    data.update({'Model': title})
+    if 'Technology' in data:
+        data['Technology'] = str(data['Technology']).strip('<a class="link-network-detail collapse" href="#"></a>')
+    print data
+
+    item.meta = dumps(data, cls=PythonObjectEncoder)
+    session.commit()

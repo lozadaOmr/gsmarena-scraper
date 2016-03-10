d04a7b64326ae845e1860cb6be148ce2c3fdfec4
Carl Isip <carl.loiuse@gmail.com>
2016-03-09 18:14:53 -0800

    added mobosdata scraper

--- managers/scrapers.py
+++ managers/scrapers.py
@@ -18,7 +18,7 @@ class ScraperManager(object):
             return None
 
         parsed_uri = urlparse(url)
-        domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+        domain = '{uri.scheme}://{uri.netloc}'.format(uri=parsed_uri)
 
         if domain not in self.scraper_providers:
             return None
@@ -29,7 +29,7 @@ class ScraperManager(object):
 scraper_manager = ScraperManager()
 
 
-@scraper_manager.add_scraper_provider("http://www.gsmarena.com/")
+@scraper_manager.add_scraper_provider("http://www.gsmarena.com")
 def gsmarena(url):
     # get response
     print "gsmarena search: %s" % url
@@ -43,7 +43,8 @@ def gsmarena(url):
     soup = BeautifulSoup(response.text, 'lxml')
     data = {}
 
-    for t in soup.select('table'):
+    # [TODO] change the implementation to dict type instead of list
+    for t in soup.select('#specs-list table'):
         h = []
 
         for e in t.select('.ttl > a'):
@@ -68,6 +69,43 @@ def gsmarena(url):
     if 'Technology' in data:
         data['Technology'] = str(data['Technology']).strip(
             '<a class="link-network-detail collapse" href="#"></a>')
+
+    print "return: %s" % data
+
+    return data
+
+
+@scraper_manager.add_scraper_provider("http://www.mobosdata.com")
+def mobosdata(url):
+    # get response
+    print "mobosdata search: %s" % url
+
+    response = requests.get(url, headers=DEFAULT_HEADERS)
+
+    if response.status_code != 200:
+        print "warning:", response.content
+        return
+
+    soup = BeautifulSoup(response.text, 'lxml')
+    data = {}
+
+    for row in soup.select('.single-phone table tr'):
+        name = row.select('th')[0].contents[0]
+        value = row.select('td')[0].contents
+
+        if not value:
+            value = ''
+        elif str(value[0]) == '<div class="fa fa-check"></div>':
+            value = True
+        elif str(value[0]) == '<div class="fa fa-times"></div>':
+            value = False
+        else:
+            value = value[0]
+
+        data[name] = value
+
+    # [TODO] Create mapping to DEFAULT_FIELDNAMES
+    #!---
 
     print "return: %s" % data
 
